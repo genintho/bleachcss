953430acf03864ac540bd86d8b1b2dd8a65b34f7
Thomas Genin <xt6.thomas.genin@gmail.com>
2018-07-13 22:06:47 -0700

    Server files

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,54 @@
-import * as http from "http";
+import * as path from "path";
+import * as fs from "fs";
+import loggerFactory from "./lib/logger";
+import express = require("express");
+import cors = require("cors");
+import bodyParser = require("body-parser");
+// import createJob from "./lib/jobQueue/createJob";
 
-let reqCnt = 1;
+const logger = loggerFactory("server");
+const app = express();
+const probeJsContent = fs.readFileSync(
+    path.resolve(__dirname, "../../probe/dist/browser/probe.js")
+);
+const probeJsContentMin = fs.readFileSync(
+    path.resolve(__dirname, "../../probe/dist/browser/probe.min.js")
+);
 
-http
-  .createServer((req, res) => {
-    const message = `Request Countss: ${reqCnt}`;
+app.get("/", (req, res) => {
+    res.send("FY");
+});
 
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(`<html><head><meta http-equiv="refresh" content="2"></head><body>${message}</body></html>`);
+app.get("/probe.js", (req, res) => {
+    res.set("Content-Type", "application/javascript");
+    res.send(probeJsContent);
+});
 
-    console.log("handled request: " + reqCnt++);
-  })
-  .listen(3000);
+app.get("/probe.min.js", (req, res) => {
+    res.set("Content-Type", "application/javascript");
+    res.send(probeJsContentMin);
+});
 
-console.log("server running on port 3000");
+app.get("/api/v1/probes", (req, res) => {
+    res.send("FY");
+});
+
+// app.post("/api/v1/probes", [cors(), bodyParser.text()], (req, res) => {
+//     logger.info("API Hit");
+//     createJob("probe_report", req.body);
+//     res.send("ack");
+// });
+
+app.listen(3000, () => console.log("WebServer is Ready to go!"));
+
+// Handle any uncaught Exception. This should never been happening in production,
+// but it is really usefull in dev
+process.on("uncaughtException", function(err) {
+    logger.error(err.toString());
+});
+
+// Handle any unhandled promise rejection. This should never been happening in production,
+// but it is really usefull in dev
+process.on("unhandledRejection", function(err) {
+    console.error("Error", err);
+});
